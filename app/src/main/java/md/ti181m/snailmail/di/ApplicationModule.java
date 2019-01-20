9f3ddc76cb9694d37f5b2977f38a324db2dc512c
Artyom Yazinsky <iw2snl@gmail.com>
2019-01-20 16:04:19 +0200

    Close the scope when activity dies

--- app/src/main/java/md/ti181m/snailmail/di/ApplicationModule.java
+++ app/src/main/java/md/ti181m/snailmail/di/ApplicationModule.java
@@ -2,14 +2,13 @@ package md.ti181m.snailmail.di;
 
 import android.app.Application;
 
-import md.ti181m.snailmail.SnailMailApplication;
 import md.ti181m.snailmail.network.SnailMailApi;
 import md.ti181m.snailmail.utils.Prefs;
 import toothpick.config.Module;
 
 public class ApplicationModule extends Module {
 
-    public ApplicationModule(SnailMailApplication application) {
+    public ApplicationModule(Application application) {
         bind(Application.class).toInstance(application);
 
         bind(SnailMailApi.class).toInstance(new SnailMailApi(application));
--- app/src/main/java/md/ti181m/snailmail/di/Dependencies.java
+++ app/src/main/java/md/ti181m/snailmail/di/Dependencies.java
@@ -1,7 +1,11 @@
 package md.ti181m.snailmail.di;
 
+import android.app.Application;
+
 import androidx.appcompat.app.AppCompatActivity;
-import md.ti181m.snailmail.SnailMailApplication;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleObserver;
+import androidx.lifecycle.OnLifecycleEvent;
 import toothpick.Scope;
 import toothpick.Toothpick;
 import toothpick.config.Module;
@@ -11,7 +15,7 @@ public final class Dependencies {
     private Dependencies() {
     }
 
-    public static void inject(SnailMailApplication application, Module... modules) {
+    public static void inject(Application application, Module... modules) {
         Scope scope = Toothpick.openScope(application);
         scope.installModules(modules);
         Toothpick.inject(application, scope);
@@ -21,5 +25,21 @@ public final class Dependencies {
         Scope scope = Toothpick.openScopes(activity.getApplication(), activity);
         scope.installModules(modules);
         Toothpick.inject(activity, scope);
+
+        activity.getLifecycle().addObserver(new ScopeCloser(activity));
+    }
+
+    private static class ScopeCloser implements LifecycleObserver {
+
+        private final Object name;
+
+        private ScopeCloser(Object name) {
+            this.name = name;
+        }
+
+        @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+        public void onDestroy() {
+            Toothpick.closeScope(name);
+        }
     }
 }
